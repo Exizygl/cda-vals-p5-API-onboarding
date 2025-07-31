44cb33ecabf25ee61d77b99a90c73dab1f3decb0
Exizygl <exizelyglaya@gmail.com>
2025-07-31 11:56:34 +0200

    feat: add import to the ORM in the different entity module

--- src/modules/campus/campus.module.ts
+++ src/modules/campus/campus.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CampusController } from './campus.controller';
 import { CampusService } from './campus.service';
+import { Campus } from './campus.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Campus])],
   controllers: [CampusController],
   providers: [CampusService]
 })
--- src/modules/formation/formation.module.ts
+++ src/modules/formation/formation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FormationService } from './formation.service';
 import { FormationController } from './formation.controller';
+import { Formation } from './formation.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Formation])],
   providers: [FormationService],
   controllers: [FormationController]
 })
