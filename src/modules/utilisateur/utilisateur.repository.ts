d781e4d901d49b54a7c6b7db60ec8a57cefa8a16
Exizygl <exizelyglaya@gmail.com>
2025-07-31 14:54:24 +0200

    chore: creation of the DTO, interface, reposition and the mappers

---
+++ src/modules/utilisateur/utilisateur.repository.ts
@@ -0,0 +1,26 @@
+// utilisateur.service.ts
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Utilisateur } from './utilisateur.entity';
+
+@Injectable()
+export class UtilisateurService {
+  constructor(
+    @InjectRepository(Utilisateur)
+    private readonly utilisateurRepo: Repository<Utilisateur>,
+  ) {}
+
+  async findAll(): Promise<Utilisateur[]> {
+    return this.utilisateurRepo.find();
+  }
+
+  async findOne(id: string): Promise<Utilisateur | null> {
+    return this.utilisateurRepo.findOne({ where: { id } });
+  }
+
+  async create(user: Utilisateur): Promise<Utilisateur> {
+    return this.utilisateurRepo.save(user);
+  }
+}
