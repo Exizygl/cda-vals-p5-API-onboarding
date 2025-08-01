772a279e9c97b336824177dbd70b5aa6daaef8dd
Exizygl <exizelyglaya@gmail.com>
2025-08-01 12:34:25 +0200

    chore: creation of DTO,mapper,interface and repository of utilisateur

--- src/modules/utilisateur/interfaces/IUtilisateurService.ts
+++ src/modules/utilisateur/interfaces/IUtilisateurService.ts
@@ -1,26 +1,5 @@
-// utilisateur.service.ts
+import { Utilisateur } from '../utilisateur.entity';
 
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Utilisateur } from '../entities/utilisateur.entity';
-
-@Injectable()
-export class UtilisateurService {
-  constructor(
-    @InjectRepository(Utilisateur)
-    private readonly utilisateurRepo: Repository<Utilisateur>,
-  ) {}
-
-  async findAll(): Promise<Utilisateur[]> {
-    return this.utilisateurRepo.find();
-  }
-
-  async findOne(id: string): Promise<Utilisateur | null> {
-    return this.utilisateurRepo.findOne({ where: { id } });
-  }
-
-  async create(user: Utilisateur): Promise<Utilisateur> {
-    return this.utilisateurRepo.save(user);
-  }
+export interface IUtilisateurService {
+  
 }
--- src/modules/utilisateur/utilisateur.repository.ts
+++ src/modules/utilisateur/utilisateur.repository.ts
@@ -1,5 +1,3 @@
-// utilisateur.service.ts
-
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
