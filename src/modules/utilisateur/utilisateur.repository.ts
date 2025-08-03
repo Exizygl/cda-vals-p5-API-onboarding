948ac02b612f7e9af4db47ed145283cdba1242a8
Exizygl <exizelyglaya@gmail.com>
2025-08-03 15:37:15 +0200

    fix(utilisateur.repository): remove useless code

--- src/modules/utilisateur/utilisateur.repository.ts
+++ src/modules/utilisateur/utilisateur.repository.ts
@@ -1,24 +1,11 @@
+
+
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Utilisateur } from './utilisateur.entity';
 
 @Injectable()
-export class UtilisateurService {
-  constructor(
-    @InjectRepository(Utilisateur)
-    private readonly utilisateurRepo: Repository<Utilisateur>,
-  ) {}
-
-  async findAll(): Promise<Utilisateur[]> {
-    return this.utilisateurRepo.find();
+export class utilisateurRepository {
+ 
   }
-
-  async findOne(id: string): Promise<Utilisateur | null> {
-    return this.utilisateurRepo.findOne({ where: { id } });
-  }
-
-  async create(user: Utilisateur): Promise<Utilisateur> {
-    return this.utilisateurRepo.save(user);
-  }
-}
