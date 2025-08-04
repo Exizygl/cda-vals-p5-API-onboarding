96df1878bd3307ca426898b58db324bda9c5e670
Exizygl <exizelyglaya@gmail.com>
2025-08-04 20:35:30 +0200

    feat: add the Role module import

--- src/modules/utilisateur/utilisateur.module.ts
+++ src/modules/utilisateur/utilisateur.module.ts
@@ -3,10 +3,13 @@ import { UtilisateurService } from './utilisateur.service';
 import { UtilisateurController } from './utilisateur.controller';
 import { Utilisateur } from './utilisateur.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleModule } from '../role/role.module';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Utilisateur])],
+  imports: [TypeOrmModule.forFeature([Utilisateur]),
+  RoleModule,
+],
   providers: [UtilisateurService],
   controllers: [UtilisateurController]
 })
