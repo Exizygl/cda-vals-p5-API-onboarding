4e378f0135c3ff322eb78306953444bc0cf56f2d
Exizygl <exizelyglaya@gmail.com>
2025-08-04 20:34:34 +0200

    fix: update the file with the new DTO

--- src/modules/utilisateur/utilisateur.controller.ts
+++ src/modules/utilisateur/utilisateur.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post, Get } from '@nestjs/common';
 import { UtilisateurService } from './utilisateur.service';
-import { UtilisateurDto } from './dto/utilisateur.dto';
+import { CreateUtilisateurDto } from './dto/createUtilisateur.dto';
 
 @Controller('utilisateurs')
 export class UtilisateurController {
   constructor(private readonly utilisateurService: UtilisateurService) {}
 
   @Post()
-  async create(@Body() dto: UtilisateurDto) {
+  async create(@Body() dto: CreateUtilisateurDto) {
     return this.utilisateurService.create(dto);
   }
 
