bd5b5eb622ba0ee53f1ed839acc09f45562310b3
Exizygl <exizelyglaya@gmail.com>
2025-08-05 13:59:46 +0200

    feat(test-app.module): change to global variable

---
+++ src/test-app.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// All your feature modules
+import { UtilisateurModule } from './modules/utilisateur/utilisateur.module';
+import { RoleModule } from './modules/role/role.module';
+import { IdentificationModule } from './modules/identification/identification.module';
+import { FormationModule } from './modules/formation/formation.module';
+import { CampusModule } from './modules/campus/campus.module';
+import { StatutPromoModule } from './modules/statut-promo/statut-promo.module';
+import { StatutIdentificationModule } from './modules/statut-identification/statut-identification.module';
+import { PromoModule } from './modules/promo/promo.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.test',
+    }),
+
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        autoLoadEntities: true,
+    }),
+
+    UtilisateurModule,
+    RoleModule,
+    IdentificationModule,
+    FormationModule,
+    CampusModule,
+    StatutPromoModule,
+    StatutIdentificationModule,
+    PromoModule,
+  ],
+})
+export class TestAppModule {}
