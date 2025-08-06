403009c610c63b9501b8bab00db9073be6f2114a
Exizygl <exizelyglaya@gmail.com>
2025-08-06 13:49:16 +0200

    feat(utilisateur.controlleur) : update user route add

--- src/modules/utilisateur/utilisateur.controller.ts
+++ src/modules/utilisateur/utilisateur.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param} from '@nestjs/common';
 import { UtilisateurService } from './utilisateur.service';
 import { CreateUtilisateurDto } from './dto/createUtilisateur.dto';
+import { UpdateUtilisateurDto } from './dto/updateUtilisateur.dto';
 
 @Controller('utilisateurs')
 export class UtilisateurController {
@@ -11,5 +12,8 @@ export class UtilisateurController {
     return this.utilisateurService.create(dto);
   }
 
-
+   @Patch(':id')
+  async update(@Param('id') id: string, @Body() dto: UpdateUtilisateurDto) {
+    return this.utilisateurService.update(id, dto);
+  }
 }
