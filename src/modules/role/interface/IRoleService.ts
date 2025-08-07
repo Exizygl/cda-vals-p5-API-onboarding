d8f4f233789d2b31224665c1976d4ffee5854597
Exizygl <exizelyglaya@gmail.com>
2025-08-07 17:50:19 +0200

    feat: role service interface  done

--- src/modules/role/interface/IRoleService.ts
+++ src/modules/role/interface/IRoleService.ts
@@ -1,9 +1,13 @@
-import { RoleDto } from '../dto/role.dto';
 import { Role } from '../role.entity';
-
+import { CreateRoleDto } from '../dto/createRole.dto';
+import { UpdateRoleDto } from '../dto/updateRole.dto';
 
 export interface IRoleService {
-      create(dto: RoleDto): Promise<Role>;
-      findByIds(ids: string[]): Promise<Role[]> 
-  
+  findByIds(ids: string[]): Promise<Role[]>;
+  findAll(): Promise<Role[]>;
+  findSelectionnable(): Promise<Role[]>;
+  findOne(id: string): Promise<Role>;
+  create(dto: CreateRoleDto): Promise<Role>;
+  update(id: string, dto: UpdateRoleDto): Promise<Role>;
+  remove(id: string): Promise<void>;
 }
