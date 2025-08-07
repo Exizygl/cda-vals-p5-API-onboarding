091d40a62c7bb54293dbecd9d6507b4c9b91f09d
SauroMatheo <sauromatheo@gmail.com>
2025-08-07 14:04:17 +0200

    fix(config-bot): add missing update and delete in service

--- src/modules/config-bot/interface/IConfigBotService.ts
+++ src/modules/config-bot/interface/IConfigBotService.ts
@@ -5,4 +5,6 @@ export interface IConfigBotService {
   findAll(): Promise<ConfigBot[]>;
   findOne(configId: number): Promise<ConfigBot | null>;
   create(configBot: ConfigBot): Promise<ConfigBot>;
+  update(configId: number, configBot: Partial<ConfigBot>): Promise<ConfigBot | null>;
+  delete(configId: number): Promise<void>;
 }
