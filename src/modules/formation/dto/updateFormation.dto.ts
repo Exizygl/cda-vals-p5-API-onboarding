4f0474f2a4cc0499dd084084b4dede5625d74f37
Exizygl <exizelyglaya@gmail.com>
2025-08-08 15:47:00 +0200

    feat: formation dto done

---
+++ src/modules/formation/dto/updateFormation.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, Matches, Length, IsBoolean, IsOptional } from 'class-validator';
+
+export class UpdateFormationDto {
+  @IsString()
+  @Length(1, 50)
+  @Matches(/^[\p{L}\s'-]+$/u, {
+    message: 'Le nom ne doit contenir que des lettres, espaces, apostrophes ou tirets.',
+  })
+  @IsOptional()
+  nom?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  selectionnable?: boolean;
+}
