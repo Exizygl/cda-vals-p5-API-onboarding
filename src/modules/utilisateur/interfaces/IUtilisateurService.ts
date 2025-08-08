fd8df23cc0599d26192f9667bc467ed33c962cad
Exizygl <exizelyglaya@gmail.com>
2025-08-08 14:33:11 +0200

    feat: interface update

--- src/modules/utilisateur/interfaces/IUtilisateurService.ts
+++ src/modules/utilisateur/interfaces/IUtilisateurService.ts
@@ -1,7 +1,11 @@
 import { CreateUtilisateurDto } from '../dto/createUtilisateur.dto';
+import { UpdateUtilisateurDto } from '../dto/updateUtilisateur.dto';
+import { UpdateRolesUtilisateurDto } from '../dto/updateRolesUtilisateur.dto';
 import { Utilisateur } from '../utilisateur.entity';
 
 export interface IUtilisateurService {
   create(dto: CreateUtilisateurDto): Promise<Utilisateur>;
-
+  update(id: string, dto: UpdateUtilisateurDto): Promise<Utilisateur>;
+  addRoles(id: string, dto: UpdateRolesUtilisateurDto): Promise<Utilisateur>;
+  removeRoles(id: string, dto: UpdateRolesUtilisateurDto): Promise<Utilisateur>;
 }
