7a75a42d7e1cf92b595985268fe3976521f40953
Exizygl <exizelyglaya@gmail.com>
2025-08-10 16:06:16 +0200

    feat: statut promo mapper done

---
+++ src/modules/statut-promo/statutPromo.mapper.ts
@@ -0,0 +1,19 @@
+import { StatutPromo } from './statutPromo.entity';
+import { CreateStatutPromoDto } from './dto/createStatutPromo.dto';
+import { UpdateStatutPromoDto } from './dto/updateStatutPromo.dto';
+
+export class StatutPromoMapper {
+  static fromCreateDto(dto: CreateStatutPromoDto): StatutPromo {
+    const statutPromo = new StatutPromo();
+    statutPromo.id = dto.id;
+    statutPromo.libelle = dto.libelle;
+    return statutPromo;
+  }
+
+  static fromUpdateDto(dto: UpdateStatutPromoDto, existingStatutPromo: StatutPromo): StatutPromo {
+    if (dto.libelle !== undefined) {
+      existingStatutPromo.libelle = dto.libelle;
+    }
+    return existingStatutPromo;
+  }
+}
