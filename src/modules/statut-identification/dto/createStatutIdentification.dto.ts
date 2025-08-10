64a5861beb86c29ecb72f20eaf5140ba4cde272e
Exizygl <exizelyglaya@gmail.com>
2025-08-10 16:26:57 +0200

    feat: statut identification dto done

---
+++ src/modules/statut-identification/dto/createStatutIdentification.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, Matches, IsNotEmpty, Length, IsBoolean} from 'class-validator';
+
+export class CreateStatutIdentificationDto {
+  @IsString()
+  @Matches(/^\d+$/, { message: 'Id doit avoir que des characteres numérique' })
+  @IsNotEmpty()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 50)
+  @Matches(/^[\p{L}\s'-]+$/u, {
+    message: 'Le libelle ne doit contenir que des lettres, espaces, apostrophes ou tirets.',
+  })
+  libelle: string;
+}
--- src/modules/statut-identification/dto/statut-identification.dto.ts
+++
@@ -1,4 +0,0 @@
-export class StatutIdentificationDto {
-  id: string;
-  libelle: string;
-}
