ed586f71152c6b2da8a338cce6e565f5c1191dca
Exizygl <exizelyglaya@gmail.com>
2025-08-10 14:48:07 +0200

    feat: campus repository update

--- src/modules/campus/campus.repository.ts
+++ src/modules/campus/campus.repository.ts
@@ -5,20 +5,5 @@ import { Campus } from './campus.entity';
 
 @Injectable()
 export class CampusService {
-  constructor(
-    @InjectRepository(Campus)
-    private readonly campusRepo: Repository<Campus>,
-  ) {}
-
-  async findAll(): Promise<Campus[]> {
-    return this.campusRepo.find();
-  }
-
-  async findOne(id: string): Promise<Campus | null> {
-    return this.campusRepo.findOne({ where: { id } });
-  }
-
-  async create(user: Campus): Promise<Campus> {
-    return this.campusRepo.save(user);
-  }
+  
 }
