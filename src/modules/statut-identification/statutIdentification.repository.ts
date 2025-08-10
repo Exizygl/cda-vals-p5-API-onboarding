213b6d3120c3efc4c8e29d0a8f49e0c0dad1d91a
Exizygl <exizelyglaya@gmail.com>
2025-08-10 16:33:36 +0200

    feat: statut identification repository update

---
+++ src/modules/statut-identification/statutIdentification.repository.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { StatutIdentification } from './statutIdentification.entity';
+
+@Injectable()
+export class StatutIdentificationService {
+
+
+}
