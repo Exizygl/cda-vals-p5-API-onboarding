6680ccebf7a108e37fb7b23a9e2614898f2cadca
Exizygl <exizelyglaya@gmail.com>
2025-08-10 16:32:50 +0200

    feat: statut identification mapper update

---
+++ src/modules/statut-identification/statutIdentification.mapper.ts
@@ -0,0 +1,19 @@
+import { StatutIdentification } from './statutIdentification.entity';
+import { CreateStatutIdentificationDto } from './dto/createStatutIdentification.dto';
+import { UpdateStatutIdentificationDto } from './dto/updateStatutIdentification.dto';
+
+export class StatutIdentificationMapper {
+  static fromCreateDto(dto: CreateStatutIdentificationDto): StatutIdentification {
+    const statutIdentification = new StatutIdentification();
+    statutIdentification.id = dto.id;
+    statutIdentification.libelle = dto.libelle;
+    return statutIdentification;
+  }
+
+  static fromUpdateDto(dto: UpdateStatutIdentificationDto, existingStatutIdentification: StatutIdentification): StatutIdentification {
+    if (dto.libelle !== undefined) {
+      existingStatutIdentification.libelle = dto.libelle;
+    }
+    return existingStatutIdentification;
+  }
+}
