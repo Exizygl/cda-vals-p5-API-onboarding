f3409a3b503a5ce1565c7dd6cbe06c6a4112a7f8
Exizygl <exizelyglaya@gmail.com>
2025-08-11 14:21:42 +0200

    feat: promo interface done

--- src/modules/promo/interface/IPromoService.ts
+++ src/modules/promo/interface/IPromoService.ts
@@ -1,5 +1,16 @@
+import { CreatePromoDto } from '../dto/createPromo.dto';
+import { UpdatePromoDto } from '../dto/updatePromo.dto';
 import { Promo } from '../promo.entity';
 
 export interface IPromoService {
-  
+    findByIds(ids: string[]): Promise<Promo[]>;
+    findBySnowflakes(snowflakes: string[]): Promise<Promo[]>;
+    findAll(): Promise<Promo[]>;
+    findActif(): Promise<Promo[]>;
+    findOne(id: string): Promise<Promo>;
+    findOneBySnowflake(snowflake: string): Promise<Promo>;
+    findPromoToStart(): Promise<Promo | null>;
+    findPromoToArchive(): Promise<Promo | null>;
+    create(dto: CreatePromoDto): Promise<Promo>;
+    update(id: string, dto: UpdatePromoDto): Promise<Promo>;
 }
