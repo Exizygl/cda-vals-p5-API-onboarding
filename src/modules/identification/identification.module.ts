b32c40750deb684e84b60afd2011ce9b76129697
Exizygl <exizelyglaya@gmail.com>
2025-08-12 16:12:19 +0200

    feat: identification update

--- src/modules/identification/identification.module.ts
+++ src/modules/identification/identification.module.ts
@@ -3,9 +3,15 @@ import { IdentificationService } from './identification.service';
 import { IdentificationController } from './identification.controller';
 import { Identification } from './identification.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StatutIdentification } from '../statut-identification/statutIdentification.entity';
+import { Promo } from '../promo/promo.entity';
+import { Utilisateur } from '../utilisateur/utilisateur.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Identification])],
+  imports: [TypeOrmModule.forFeature([Identification,
+      StatutIdentification, // <- This is the one missing
+      Promo,
+      Utilisateur,])],
   providers: [IdentificationService],
   controllers: [IdentificationController]
 })
