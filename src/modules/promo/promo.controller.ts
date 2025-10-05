0be80c58d7dfcd965b8727eb37c3c8ae7ddb1286
Exizygl <exizelyglaya@gmail.com>
2025-10-05 17:19:43 +0200

    feat: add missing path

--- src/modules/promo/promo.controller.ts
+++ src/modules/promo/promo.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Patch, Delete, Param, Body, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Body,
+  Param,
+  Query,
+  ParseArrayPipe,
+  Inject,
+} from '@nestjs/common';
 import { IPromoService } from './interface/IPromoService';
 import { IPromoServiceToken } from './promo.constants';
 import { CreatePromoDto } from './dto/createPromo.dto';
@@ -18,29 +28,66 @@ export class PromoController {
     return this.promoService.findAll();
   }
 
+
   @Get('actif')
   async findActif(): Promise<Promo[]> {
     return this.promoService.findActif();
   }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Promo> {
-    return this.promoService.findOne(id);
+
+  @Get('to-start')
+  async findPromoToStart(): Promise<Promo[] | null> {
+    return this.promoService.findPromoToStart();
   }
+
+
+  @Get('to-archive')
+  async findPromoToArchive(): Promise<Promo[] | null> {
+    return this.promoService.findPromoToArchive();
+  }
+
+
+  @Get('by-ids')
+  async findByIds(
+    @Query('ids', new ParseArrayPipe({ items: String, separator: ',' }))
+    ids: string[],
+  ): Promise<Promo[]> {
+    return this.promoService.findByIds(ids);
+  }
+
+
+  @Get('by-snowflakes')
+  async findBySnowflakes(
+    @Query('snowflakes', new ParseArrayPipe({ items: String, separator: ',' }))
+    snowflakes: string[],
+  ): Promise<Promo[]> {
+    return this.promoService.findBySnowflakes(snowflakes);
+  }
+
 
   @Get('snowflake/:snowflake')
   async findOneBySnowflake(@Param('snowflake') snowflake: string): Promise<Promo> {
     return this.promoService.findOneBySnowflake(snowflake);
   }
 
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Promo> {
+    return this.promoService.findOne(id);
+  }
+
+
   @Post()
   async create(@Body() dto: CreatePromoDto): Promise<Promo> {
     return this.promoService.create(dto);
   }
 
+
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() dto: UpdatePromoDto): Promise<Promo> {
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdatePromoDto,
+  ): Promise<Promo> {
     return this.promoService.update(id, dto);
   }
-
 }
